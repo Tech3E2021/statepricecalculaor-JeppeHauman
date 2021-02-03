0f08da35013bedcc28fd4212bb2d80ec0d742185
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-02-03 12:39:01 +0100

    Update Program.cs

--- StatePriceCalculator/Program.cs
+++ StatePriceCalculator/Program.cs
@@ -9,25 +9,21 @@ namespace StatePriceCalculator
             //Console.WriteLine("Hello World!");
 
             //Instance fields
-            double NumberOfItems;
-            double PriceOfItems;
-            double TotalPrice;
-            double TotalPriceDiscounted = 0;
-            string StateCode;
-            double TotalPriceTaxed = 0;
+            double totalPriceDiscounted = 0;
+            double totalPriceTaxed = 0;
             //En velkommen besked til brugeren
-            Console.WriteLine("Velkommen til 'THE BEST CALCULATOR EVER'");
+            Console.WriteLine("Welcome to 'THE BEST CALCULATOR EVER'");
 
             //Brugeren taster antallet af varer
             Console.WriteLine("Skriv antallet af varer");
-            NumberOfItems = Convert.ToInt32(Console.ReadLine());
+            double numberOfItems = Convert.ToInt32(Console.ReadLine());
 
             //Brugeren taster prisen på varen
             Console.WriteLine("Skriv prisen på varen");
-            PriceOfItems = Convert.ToInt32(Console.ReadLine());
+            double priceOfItems = Convert.ToInt32(Console.ReadLine());
 
             //Giv brugeren besked om samlet pris
-            TotalPrice = NumberOfItems * PriceOfItems;
+            var TotalPrice = numberOfItems * priceOfItems;
             Console.WriteLine($"Den samlede pris uden discount og tax er: {TotalPrice}");
 
             //Udregn samlet pris med discount
@@ -35,63 +31,63 @@ namespace StatePriceCalculator
             {
                 Console.WriteLine("Der er ingen discount");
             }
-			    else if (TotalPrice >=1000 && TotalPrice < 4999)
-			    {
-				    TotalPriceDiscounted = TotalPrice * 0.97;
-				    Console.WriteLine("Discount: 3%");
-			    }
-                    else if (TotalPrice >=5000 && TotalPrice < 6999)
-                    {
-                        TotalPriceDiscounted = TotalPrice * 0.95;
-                        Console.WriteLine("Discount: 5%");
-                    }
-                        else if (TotalPrice >= 7000 && TotalPrice < 9999)
-                        {
-                            TotalPriceDiscounted = TotalPrice * 0.93;
-                            Console.WriteLine("Discount: 7%");
-                        }
-                            else if (TotalPrice >= 10000 && TotalPrice < 49999)
-                            {
-                                TotalPriceDiscounted = TotalPrice * 0.90;
-                                Console.WriteLine("Discount: 10%");
-                            }
-                                else
-                                {
-                                    TotalPriceDiscounted = TotalPrice * 0.85;
-                                    Console.WriteLine("Discount: 15%");
-                                }
+            else if (TotalPrice >=1000 && TotalPrice < 4999)
+            {
+                totalPriceDiscounted = TotalPrice * 0.97;
+                Console.WriteLine("Discount: 3%");
+            }
+            else if (TotalPrice >=5000 && TotalPrice < 6999)
+            {
+                totalPriceDiscounted = TotalPrice * 0.95;
+                Console.WriteLine("Discount: 5%");
+            }
+            else if (TotalPrice >= 7000 && TotalPrice < 9999)
+            {
+                totalPriceDiscounted = TotalPrice * 0.93;
+                Console.WriteLine("Discount: 7%");
+            }
+            else if (TotalPrice >= 10000 && TotalPrice < 49999)
+            {
+                totalPriceDiscounted = TotalPrice * 0.90;
+                Console.WriteLine("Discount: 10%");
+            }
+            else
+            {
+                totalPriceDiscounted = TotalPrice * 0.85;
+                Console.WriteLine("Discount: 15%");
+            }
 
 
             //Besked om samlet pris med discount
-            Console.WriteLine($"Den samlede pris med discount er: {TotalPriceDiscounted} ");
+            Console.WriteLine($"Den samlede pris med discount er: {totalPriceDiscounted} ");
 
             //Besked: Statecode på 2 bogstaver
             Console.WriteLine("Skriv din state code(UT, NV, TX, AL, CA)");
 
             //Brugeren taster State-Code, 2 bogstaver
-            StateCode = Console.ReadLine();
+            var stateCode = Console.ReadLine();
 
             //udregn pris udfra statens tax
-            switch (StateCode)
+            switch (stateCode)
             {
                 case "UT":
-                    TotalPriceTaxed = TotalPriceDiscounted * 1.0685;
+                    totalPriceTaxed = totalPriceDiscounted * 1.0685;
                     Console.WriteLine("Din state tax: 6,85%");
                     break;
                 case "NV":
-                    TotalPriceTaxed = TotalPriceDiscounted * 1.08;
+                    totalPriceTaxed = totalPriceDiscounted * 1.08;
                     Console.WriteLine("Din state tax: 8%");
                     break;
                 case "TX":
-                    TotalPriceTaxed = TotalPriceDiscounted * 1.0625;
+                    totalPriceTaxed = totalPriceDiscounted * 1.0625;
                     Console.WriteLine("Din state tax: 6,25%");
                     break;
                 case "AL":
-                    TotalPriceTaxed = TotalPriceDiscounted * 1.04;
+                    totalPriceTaxed = totalPriceDiscounted * 1.04;
                     Console.WriteLine("Din state tax: 4%");
                     break;
                 case "CA":
-                    TotalPriceTaxed = TotalPriceDiscounted * 1.0825;
+                    totalPriceTaxed = totalPriceDiscounted * 1.0825;
                     Console.WriteLine("Din state tax: 8,25%");
                     break;
                 default:
@@ -101,7 +97,7 @@ namespace StatePriceCalculator
 
             //Besked til brugeren om samlet pris efter tax og discount
 
-            Console.WriteLine($"Din samlede pris efter tax: {TotalPriceTaxed}");
+            Console.WriteLine($"Din samlede pris efter tax: {totalPriceTaxed}");
 
 
 
